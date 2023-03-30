9cd4df2652f6426e2f67f825c950f7b4fb361228
Andrey Volkov <andreyf.volkov@yandex.ru>
2023-03-30 21:10:10 +0300

    нв вторую проверку после замечаний ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,17 @@
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditPopup = document.querySelector(".popup_type_profile-edit");
-const profileEditPopupClose = profileEditPopup.querySelector(".popup__close_type_profile-edit");
-const profileEditPopupForm = profileEditPopup.querySelector(".popup__form_type_profile-edit");
+const profileEditPopupClose = profileEditPopup.querySelector(".popup__close");
+const profileEditPopupForm = profileEditPopup.querySelector(".popup__form");
 const inputName = profileEditPopup.querySelector(".popup__input_type_name");
 const inputDescription = profileEditPopup.querySelector(".popup__input_type_description");
-const profileEditPopupSubmit = profileEditPopup.querySelector(".popup__bottom-submit_type_profile-edit");
+const profileEditPopupSubmit = profileEditPopup.querySelector(".popup__bottom-submit");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
 profileEditButton.addEventListener("click", function() {
 profileEditPopup.classList.add("popup_opened");
-inputName.value = profileName.innerHTML;
-inputDescription.value = profileDescription.innerHTML;
+inputName.value = profileName.textContent;
+inputDescription.value = profileDescription.textContent;
 })
 
 profileEditPopupClose.addEventListener("click", function() {
@@ -20,7 +20,7 @@ profileEditPopup.classList.remove("popup_opened");
 
 profileEditPopupForm.addEventListener("submit", function (event) {
 event.preventDefault();
-profileName.innerHTML = inputName.value;
-profileDescription.innerHTML = inputDescription.value;
+profileName.textContent = inputName.value;
+profileDescription.textContent = inputDescription.value;
 profileEditPopup.classList.remove("popup_opened");
 })
